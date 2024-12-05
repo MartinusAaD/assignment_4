83e238015046f5d5675bb18b61bd791c174a77bc
Martinus Aamot Dahl <maraad97@gmail.com>
2024-12-05 13:34:00 +0100

    Created base structure

---
+++ src/js/button.js
@@ -0,0 +1,5 @@
+const buttonContainer = document.querySelector(".component__button");
+const button = document.createElement("button");
+button.classList.add("button");
+button.textContent = "Button";
+buttonContainer.append(button);
